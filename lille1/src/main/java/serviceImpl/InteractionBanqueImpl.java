4bef5904a026b7832238840db9c17a5d1ef8861c
pyr0technicien <pyr0technici3n@gmail.com>
2015-11-30 11:58:05 +0000

    commi

--- lille1/src/main/java/serviceImpl/InteractionBanqueImpl.java
+++ lille1/src/main/java/serviceImpl/InteractionBanqueImpl.java
@@ -1,9 +1,11 @@
 package serviceImpl;
 
+import org.springframework.stereotype.Service;
+
 import model.Banque;
 import model.Client;
 
-
+@Service
 public class InteractionBanqueImpl implements service.InteractionBanque {
 
 	public Banque connecter(Client client) {
