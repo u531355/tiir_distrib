762e8b42871554fc1d89846c9d154c66714e5a20
pyr0technicien <pyr0technici3n@gmail.com>
2015-12-07 18:34:08 +0000

    Autowired => Ok

--- lille1/src/main/java/app/Application.java
+++ lille1/src/main/java/app/Application.java
@@ -1,10 +1,11 @@
-package controllers;
+package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"serviceimpl", "daoimpl", "controllers"})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
