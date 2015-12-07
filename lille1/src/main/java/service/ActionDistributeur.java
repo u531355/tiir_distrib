d9b0056d8e40e1ee8996128d79aa8e187efa2bac
blanquart <blanquart@pcmt10.m2fil.univ-lille1.fr>
2015-12-07 14:37:36 +0100

    ajout dernier service

--- lille1/src/main/java/service/ActionDistributeur.java
+++ lille1/src/main/java/service/ActionDistributeur.java
@@ -2,11 +2,12 @@ package service;
 
 import model.Client;
 import model.Distributeur;
+import model.Retrait;
 
 public interface ActionDistributeur {
 	String afficherSolde(Client client);
 
 	Distributeur retrait(Client client, double montant);
 
-	Distributeur virement(Client client, double montant, String ibanTo);
+	Retrait virement(Client client, double montant, String ibanTo);
 }
