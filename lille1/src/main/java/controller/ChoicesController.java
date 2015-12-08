549650e26726e9042a2a6e6cf75f3bd6e07dea3b
Lionel Chalet <lionel.chalet@gmail.com>
2015-12-08 11:58:51 +0100

    packages are singular

--- lille1/src/main/java/controller/ChoicesController.java
+++ lille1/src/main/java/controller/ChoicesController.java
@@ -1,4 +1,4 @@
-package controllers;
+package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- lille1/src/main/java/controller/IndexController.java
+++ lille1/src/main/java/controller/IndexController.java
@@ -1,4 +1,4 @@
-package controllers;
+package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
