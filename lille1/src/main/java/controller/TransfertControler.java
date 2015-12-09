c0919c6140d0394417fcededebce65f6564b970d
Bakiraka <anthony.djenane@gmail.com>
2015-12-09 10:50:05 +0100

    Work on transfert controller in progress

--- lille1/src/main/java/controller/TransfertControler.java
+++ lille1/src/main/java/controller/TransfertControler.java
@@ -1,11 +1,14 @@
 package controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import model.Client;
 import service.InteractionBanque;
@@ -21,15 +24,21 @@ public class TransfertControler {
 	private InteractionBanque interactionbanque;
 
 	@RequestMapping(value = "/transfert", method = RequestMethod.GET)
-	public String transfert(Model model) {
-		model.addAttribute("client", new Client());
-		model.addAttribute("connected", false);
+	public String transfert(Model model, HttpSession session) {
+		model.addAttribute(model);
 		return "transfert";
 	}
 
 	@RequestMapping(value = "/transfert", method = RequestMethod.POST)
-	public String transfert(Model model, @ModelAttribute Client client) {
-		return null;
+	public String transfert(Model model, 
+								HttpSession session, 
+								@RequestParam("accountfrom") String accountfrom,
+								@RequestParam("accountto") String accountto,
+								@RequestParam("amount") String amount) {
+		
+		model.addAttribute(model);
+		//TODO : handling the money transfert transfert
+		return "transfert";
 		
 	}
 }
