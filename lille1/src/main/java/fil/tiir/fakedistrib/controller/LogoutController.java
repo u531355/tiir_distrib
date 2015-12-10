14f3fe1472677f4eecd304143a204827a4b70220
Bakiraka <anthony.djenane@gmail.com>
2015-12-10 17:00:05 +0100

    Ajout de la fonction de suppression token dans le controller

--- lille1/src/main/java/fil/tiir/fakedistrib/controller/LogoutController.java
+++ lille1/src/main/java/fil/tiir/fakedistrib/controller/LogoutController.java
@@ -2,18 +2,38 @@ package fil.tiir.fakedistrib.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import fil.tiir.fakedistrib.entity.Client;
+import fil.tiir.fakedistrib.exception.InteractionBanqueException;
+import fil.tiir.fakedistrib.service.InteractionBanque;
+
 @Controller
 public class LogoutController {
 
+	@Autowired
+	private InteractionBanque interactionBanque;
+	
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	public String logout(Model model, HttpSession session) {
-		session.removeAttribute("client");
-		// TODO Delete Token (dans InteractionBanque)
+		
+		Client client = (Client) session.getAttribute("client");
+		if (client == null)
+			return "redirect:/";
+		try {
+			
+			interactionBanque.suppressionToken(client);
+			session.removeAttribute("client");
+			
+		} catch (InteractionBanqueException e) {
+			model.addAttribute("error", e.getMessage());
+			return "error";
+		}
+		
 		return "redirect:/";
 	}
 }
