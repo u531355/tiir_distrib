dcd13d7c8ce05787c88c52aafd08fae9c5d1ecfc
pyr0technicien <masquelier.th@gmail.com>
2015-12-13 15:15:09 +0100

    Create table

---
+++ lille1/create_distrib.sql
@@ -0,0 +1,32 @@
+﻿-- Schema: public
+
+-- DROP SCHEMA public;
+
+
+
+CREATE TABLE Banque(
+   id_banque  SERIAL PRIMARY KEY,
+   iban_start           TEXT      NOT NULL,
+   url            TEXT       NOT NULL
+);
+
+CREATE TABLE Virement(
+   id_virement  SERIAL PRIMARY KEY,
+   date           DATE      NOT NULL,
+   montant            DECIMAL       NOT NULL,
+   iban_from	TEXT 	NOT NULL,
+   iban_to	TEXT 	NOT NULL,
+   id_banque	INTEGER NOT NULL
+);
+
+CREATE TABLE Distributeur(
+   id_distributeur  SERIAL PRIMARY KEY,
+   montant           DECIMAL      NOT NULL
+);
+CREATE TABLE Retrait(
+   id_retrait  SERIAL PRIMARY KEY,
+   date           DATE      NOT NULL,
+   montant            DECIMAL       NOT NULL,
+   iban_from	TEXT 	NOT NULL,
+   id_banque	INTEGER NOT NULL
+);
